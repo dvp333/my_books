cc7b97bd1aab828fec3abc0f72b4c51e0399cd44
Diego Prado <dvp333@gmail.com>
2023-06-13 12:28:59 -0300

    Adds Equatable to states.

--- lib/app/layers/presentation/favorites/cubit/favorites_cubit.dart
+++ lib/app/layers/presentation/favorites/cubit/favorites_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_books/app/layers/domain/entities/book.dart';
